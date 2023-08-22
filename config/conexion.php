622a9b32db207580ef5d9e2196f1d7541fb8a5c9
DayronG27 <dayronex@gmail.com>
2023-08-22 14:57:05 -0600

    conexion a base de datos
    conexion a base de datos

---
+++ config/conexion.php
@@ -0,0 +1,12 @@
+<?php
+
+                     // (E) conexion local con mi servidor y  base de datos 
+$server   ="localhost";   
+$user     ="root";
+$password ="";
+$database ="Diazcard";
+
+ 
+$conn = mysqli_connect($server,$user,$password,$database);   // (T)conexion local con mi servidor y base de datos  
+
+?>
