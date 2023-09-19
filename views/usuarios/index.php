92b86b379b59e327cd419e1cc74340c2b2c3ff0c
DayronG27 <dayronex@gmail.com>
2023-09-18 20:36:59 -0600

    actualizacion 18-09-2023

--- views/usuarios/index.php
+++ views/usuarios/index.php
@@ -5,7 +5,8 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Registro personas</title>
-   <link rel="stylesheet" href="../../css/usuarios.css">
+  <link rel="stylesheet" href="../../css/usuarios.css">
+
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 
 </head>
@@ -18,9 +19,11 @@
    require_once('procesartablausuarios.php');
    ?>
 
-<div class="row">
-          <div class="container">
-        <div class="uno" >  
+<div class="container">
+             <div class="row">
+               <div class="col-sm-3" >
+              
+        
             <h1>Registro usuario</h1>
             <form action="procesarformulario.php" method="POST"> <!-- anclaje a archivo procesarformulario.php -->
 
@@ -34,35 +37,40 @@
                <input type="tel" name="celular" id="celular" placeholder="Celular" class="box">
 
                <input type="numb" name="documento" id="documento" placeholder="numero de documento" class="box">
-               <div class="select">
-                  <select class="form-control" name="tipodocumento" id="tipodocumento">
+         
+                  <select  class="form-control" name="tipodocumento" id="tipodocumento"  >
 
                      <option value="1">C.C</option>
                      <option value="2">T.I</option>
                      <option value="3">C.E</option>
 
                   </select>
-               </div>
+               
                <br> <br>
                <input class="btn btn-outline-primary" type="submit" value="Enviar" class="submit">
                </form>
+
+               </div>
+                
           
 
-             
-            <table class="table table-sm table-hover  ">
+           <div class="col-sm-6" >
+            
+            <table class="table table-sm table-hover">
                <thead class="table-dark">
                   <tr>
                      <th scape="col"> # </th>
-                     <th scape="col"> tipodocumento </th>
-                     <th scape="col"> documento </th>
                      <th scape="col"> nombre </th>
-                     <th scape="col"> celular </th>
                      <th scape="col"> correo </th>
+                     <th scape="col"> celular </th>
+                     <th scape="col"> documento </th>
+                     <th scape="col"> celular </th>
                      <th scape="col"> fecha de registro </th>
                      <th scape="col" colspan="3"> opciones </th> <!--  3 columnas diferentes -->
                   </tr>
                   <!--  otro esta es una consulta  -->
                </thead>
+               </div>
                <tbody>
                   <?php
                   if (mysqli_num_rows($resultado) == 0) {
@@ -85,7 +93,7 @@
                         $date = date_create($usuarios['fecha_registro']);
                      ?>
                         <tr>
-                           <th scope="row">
+                           <th>
                               <?= $numero_registro ?>
                            </th>
                            <td>
@@ -119,47 +127,19 @@
             </table>
          </div>
       </div>
+         </div>
+      </div>
+      </div>
       </div>
   
  
    
  
-   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous"></script>
-
-
-</body>
-
-<!--<div class="info">
-          
-          <img src="../../imgs/Persona.svg" class="img-1" alt="">
-            <div class="location">
-               <div class="icons">
-                  <img src="../../imgs/gps.svg" alt="">
-                  <p>Calle 5 # 2 - Tunja</p>
-               </div>
-        
-                <div class="icons">
-                  <img src="../../imgs/number.svg" alt="">
-                  <p>3110221111</p>
-               </div>
-        
-              <div class="icons">
-                  <img src="../../imgs/email.svg" alt="">
-                  <p>Diazcard@gmail.com</p>
-               </div>
-        </div>
    
 
-
-        <div class="social">
-           
-             <img src="../../imgs/fa.svg" alt="">
-             <img src="../../imgs/whatsaap.svg" alt="">
-             <img src="../../imgs/ins.svg" alt=""> 
-        </div>
-     </div>
- </div>
-</div>
-</div>
 </body>
-</html>
+
+
+ </html>
+
+
--- views/usuarios/procesarformulario.php
+++ views/usuarios/procesarformulario.php
@@ -1,19 +1,24 @@
 <?php
 require_once '../../config/conexion.php';  //anclaje a conexion.php  
 //aqui proceso las variables del html con php 
- 
-$tipodocumento     =($_POST['tipodocumento']);
-$documento         =trim($_POST['documento']);
-$nombre            =trim($_POST['nombre']);       //con trim elimino espacios  al inicio y al final que ingrese el usuario en la interfaz
-$celular           =trim($_POST['celular']);     //$son las variables [''] lo que ingresa el usuario en la interfaz
-$correo            =trim($_POST['correo']);      
 
-if (strlen($tipodocumento) >= 1 && strlen($documento) >= 1 &&  strlen($nombre) >= 1 && strlen($celular) >= 1  && strlen ($correo) >= 1   ){ 
-$consulta ="INSERT INTO usuarios (tipodocumento,documento,nombre,celular,correo) 
+$tipodocumento     = ($_POST['tipodocumento']);
+$documento         = trim($_POST['documento']);
+$nombre            = trim($_POST['nombre']);       //con trim elimino espacios  al inicio y al final que ingrese el usuario en la interfaz
+$celular           = trim($_POST['celular']);     //$son las variables [''] captura lo que ingresa el usuario en la interfaz
+$correo            = trim($_POST['correo']);
+
+if (strlen($nombre) >= 1 && strlen($documento) >= 1 &&  strlen($nombre) >= 1 && strlen($celular) >= 1  && strlen($correo) >= 1) {
+    $consulta = "INSERT INTO usuarios (tipodocumento,documento,nombre,celular,correo) 
 VALUES ('$tipodocumento','$documento','$nombre','$celular','$correo')";   //inserto en la tabla usuarios en los campos (), lo que el usuarios ingresa en las variables $
 
 
-$resultado = mysqli_query($conn,$consulta); //mysqli_query consulta a la base de datos
+    $resultado = mysqli_query($conn, $consulta); //mysqli_query consulta a la base de datos              
 
- }
- 
+    if ($resultado)  mysqli_query($conn, $consulta);
+    if ($result) {
+        echo "se ha guardado el registro";
+    } else {
+        echo "no se guardara en la base de datos";
+    }
+}
